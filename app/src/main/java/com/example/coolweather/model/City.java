04c0d0c820c4a9f415bc3dd14bc5a88c5de0cc82
whitejoke <852610094@qq.com>
2016-05-21 16:49:00 +0800

    Second commit

---
+++ app/src/main/java/com/example/coolweather/model/City.java
@@ -0,0 +1,43 @@
+package com.example.coolweather.model;
+
+/**
+ * Created by joker on 2016/5/20.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private String provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
